aa40cac1f483282ca6ad0234944d86a8a1ce3460
nmio <kristo.koert@gmail.com>
2019-12-23 14:01:44 +0200

    Update

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 .PHONY: apply_bolt
 apply_bolt:
 	sudo cp bolt/configuration.nix /etc/nixos/configuration.nix
+
+.PHONY: apply_workstation
+apply_workstation:
+	sudo cp workstation/configuration.nix /etc/nixos/configuration.nix
--- workstation/configuration.nix
+++ workstation/configuration.nix
@@ -16,7 +16,56 @@
   boot.loader.grub.useOSProber = true;
   # Use the latest kernel for magic touchpad 2 driver support (required 4.20+)
   # TODO: When 4.20+ becomes mainline, use that
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  # boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = let
+      linux_sgx_pkg = { fetchurl, buildLinux, ... } @ args:
+
+        buildLinux (args // rec {
+          version = "5.0.0";
+          modDirVersion = version;
+
+          src = fetchurl {
+            url = "https://github.com/torvalds/linux/archive/v5.0.tar.gz";
+            sha256 = "10c228c231bedac30e704a58344bf70a580e92a18e1ccbf8ae2004231ce32c16";
+          };
+          kernelPatches = [
+            { name = "0001";
+              patch = fetchurl {
+                url = "https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.0.21/0001-base-packaging.patch";
+                sha256 = "40a0c6bbe0087a8b082c41d443afd0129da44cf3d233562b5590f986282deed1";
+              };
+            }
+            { name = "0002";
+              patch = fetchurl {
+                url = "https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.0.21/0002-UBUNTU-SAUCE-add-vmlinux.strip-to-BOOT_TARGETS1-on-p.patch";
+                sha256 = "f8115da44cecef42c071a494285000cf6e45d7ad5e131df964a5ab781a7cba54";
+              };
+            }
+            { name = "0003";
+              patch = fetchurl {
+                url = "https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.0.21/0003-UBUNTU-SAUCE-tools-hv-lsvmbus-add-manual-page.patch";
+                sha256 = "847e7da88c7b34c397009ed27c0868709466e145ff402cf60739088c72fe5d05";
+              };
+            }
+            { name = "0004";
+              patch = fetchurl {
+                url = "https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.0.21/0004-debian-changelog.patch";
+                sha256 = "6e113103a5fa75d0b3a8e0287f7241edb4ca770096f46735bf2652c59a4e9c91";
+              };
+            }
+            { name = "0005";
+              patch = fetchurl {
+                url = "https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.0.21/0005-configs-based-on-Ubuntu-5.0.0-16.17.patch";
+                sha256 = "c37ca2d78b472dead6a3eacd3ced234af9811b1b65eedacaa2d362c0399fcd99";
+               };
+             }
+          ];
+
+          extraMeta.branch = "5.0.0";
+        } // (args.argsOverride or {}));
+      linux_sgx = pkgs.callPackage linux_sgx_pkg{};
+    in
+      pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux_sgx);
 
   networking.hostName = "nmio-workstation";
   networking.networkmanager.enable = true;
@@ -42,6 +91,8 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
+    openvpn
+
     firefox
     chromium
 
@@ -55,14 +106,14 @@
     gnumake
     binutils
     ktorrent
-    displaylink
 
     vlc
     nodejs-12_x
     rustup
     cargo
+    python37
 
-    emacs-nox
+    emacs26-nox
     jetbrains.jdk
     jetbrains.goland
     jetbrains.clion
@@ -80,7 +131,7 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   networking.firewall.allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
   networking.firewall.allowedUDPPortRanges = [ { from = 1714; to = 1764; } ];
@@ -103,9 +154,8 @@
 
   # GPUs
   services.xserver.videoDrivers = [
-        "nvidia"
-        "displaylink"
-        "modesetting"
+    "nvidia"
+    "displaylink"
   ];
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
