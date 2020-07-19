e7ffb87538c621f0cb9c9b3f63937a748095f37e
Kristo Koert <kristo.koert@gmail.com>
2020-07-19 21:26:06 +0300

    add thinclient

---
+++ thinclient/configuration.nix
@@ -0,0 +1,103 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # This machine boots in UEFI mode
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nmio-tc"; # thin client
+  networking.networkmanager.enable = true;
+
+  hardware.pulseaudio.enable = true;
+  hardware.opengl.driSupport32Bit = true;
+  hardware.pulseaudio.support32Bit = true;
+  hardware.bluetooth.enable = true;
+
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "us";
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  time.timeZone = "Europe/Tallinn";
+
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.firefox.enablePlasmaBrowserIntegration = true;
+
+  environment.systemPackages = with pkgs; [
+    # System
+    bash
+    binutils
+    docker
+    docker-compose
+    gcc
+    git
+    gnumake
+    htop
+    lazydocker
+    openssl
+    python37
+
+    # Applications
+    ark
+    barrier
+    chromium
+    emacs26-nox
+    firefox
+    kdeconnect
+    ktorrent
+    slack
+    spectacle
+    plasma-browser-integration
+    tdesktop
+    vlc
+
+    # TODO: Split into Go dev env
+    go
+    jetbrains.jdk
+    jetbrains.goland
+  ];
+
+  virtualisation.docker.enable = true;
+  virtualisation.docker.enableOnBoot = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.bash.enableCompletion = true;
+
+  # List services that you want to enable:
+  services.openssh.enable = true;
+  services.xserver.enable = true;
+  services.xserver.layout = "us";
+  services.xserver.xkbOptions = "eurosign:e";
+  services.xserver.libinput.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  # NOTE: dockerdev group is a dummy group for working with docker volumes
+  #       Both docker and host user (nmio) will have a group with guid "1024" and thus can share the data there
+  users.groups.dockerdev =
+  { name = "dockerdev";
+    gid = 1024;
+  };
+  users.users.nmio =
+  { isNormalUser = true;
+    home = "/home/nmio";
+    extraGroups = [ "wheel" "networkmanager" "docker" "dockerdev" "adbuser" ];
+    shell = pkgs.bash;
+  };
+
+  # These are the commands to change channel:
+  # - sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
+  # - sudo nixos-rebuild switch --upgrade
+  system.stateVersion = "20.03";
+}
